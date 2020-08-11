1d4047802399fc212c3f1f329f87f3060510c492
WhiteDot <65070870+whitehauler@users.noreply.github.com>
2020-08-11 10:28:11 +0530

    Add files via upload

---
+++ al.js
@@ -0,0 +1,3 @@
+alert("i have uploaded it before by my old account see apology post for more details but its not copied code its my second account and i am the author");
+var name=prompt("your name ? ");
+alert(" Hi "+name+" , scroll it to know about me and upvote if you like it");
